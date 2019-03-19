e87d93725a1d69157e9526268c6058b7c6fb28ef
Ahmad Gomaa <ahmed.gomaa@apptcom.com>
2019-03-19 04:54:22 +0200

    create controller with two methods one view and another to return  ajax request by two routes get

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Datatables;
+use App\User;
+
+class UserController extends Controller
+{
+
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Datatables::of(User::query())->make(true);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.index');
+    }
+}
